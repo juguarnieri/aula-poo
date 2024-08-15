20a8bb7a3d13a8b3a381db8575af1848396a7418
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-08-15 09:46:43 -0300

    feat: implementação dos desafios

--- aula4exercicio1.js
+++ aula4exercicio1.js
@@ -1,45 +1,62 @@
 const livroObjeto = new Object();
 
-    livroObjeto.titulo = "O Grande Desafio";
-    livroObjeto.autor = "Pedro Bandeira";
-    livroObjeto.ano = 2020
+livroObjeto.titulo = "O Grande Desafio";
+livroObjeto.autor = "Pedro Bandeira";
+livroObjeto.ano = 2020
 
-console.clear();
 console.log(livroObjeto);
 
+livroObjeto.ano = 2021
+livroObjeto.paginas = 120
+
+console.log(livroObjeto);
+
+
 const alunoObjeto = new Object();
-    alunoObjeto.nome = "Júlia";
-    alunoObjeto.turma = "Segundo Ano";
-    alunoObjeto.dataNascimento = 2007
-    
+alunoObjeto.nome = "Julia";
+alunoObjeto.turma = "Segundo Ano";
+alunoObjeto.dataNascimento = 2007
+
+alunoObjeto.nome = "Júlia";
+
 console.log(alunoObjeto);
 
+
 const carroObjeto = new Object();
-    carroObjeto.cor = "Prata";
-    carroObjeto.modelo = "HRV";
-    carroObjeto.ano = 2019
+carroObjeto.cor = "Prata";
+carroObjeto.modelo = "HRV";
+carroObjeto.ano = 2019
+
+carroObjeto.ano = 2020
 
 console.log(carroObjeto);
 
+
 const filmeObjeto = new Object();
-    filmeObjeto.titulo = "Gente Grande";
-    filmeObjeto.diretor = "Dennis Dugan";
-    filmeObjeto.anoLancamento = 2010
+filmeObjeto.titulo = "Gente Grande";
+filmeObjeto.diretor = "Dennis Dugan";
+filmeObjeto.anoLancamento = 2010
 
 console.log(filmeObjeto);
 
+
 const cachorroObjeto = new Object();
-    cachorroObjeto.nome = "Babi Cristina";
-    cachorroObjeto.raca = "Dachshund";
-    cachorroObjeto.cor = "Branca com manchas marrons";
-    cachorroObjeto.olhos = "Azul";
+cachorroObjeto.nome = "Babi Cristina";
+cachorroObjeto.raca = "Dachshund";
+cachorroObjeto.cor = "Branca com manchas marrons";
+cachorroObjeto.olhos = "Azul";
+
+cachorroObjeto.olhos = "Azul claro";
 
 console.log(cachorroObjeto);
 
+
 const produtoObjeto = new Object();
-    produtoObjeto.descricao = "Mouse preto, sem fio";
-    produtoObjeto.marca = "Multilaser";
-    produtoObjeto.preco = 39.99
+produtoObjeto.descricao = "Mouse preto, sem fio";
+produtoObjeto.marca = "Multilaser";
+produtoObjeto.preco = 39.99
+
+produtoObjeto.preco = 42.99
 
 console.log(produtoObjeto);
     
