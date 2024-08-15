aa1625a75f07c181e3e78271b4953f2d992e3bea
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-08-15 09:09:02 -0300

    feat: exercícios Back-End

---
+++ aula4exp1.js
@@ -0,0 +1,13 @@
+let pessoa = "Júlia"
+
+const pessoaObjeto = {
+    corCabelo:"Castanho",
+    corOlhos: "azul",
+    corPele:"transparente",
+    idade: 16,
+    altura: 2.5,
+    peso: 80
+};
+
+console.log(pessoa);
+console.log(pessoaObjeto);
---
+++ aula4exp2.js
@@ -0,0 +1,15 @@
+let pessoa = "Júlia"
+
+const pessoaObjeto = new Object();
+
+pessoaObjeto.nome = "Miguel"
+pessoaObjeto.idade = 17
+pessoaObjeto.altura = 1.70
+
+pessoaObjeto.idade = 18
+pessoaObjeto.altura = 1.74
+
+
+console.clear()
+console.log(pessoa)
+console.log(pessoaObjeto)
